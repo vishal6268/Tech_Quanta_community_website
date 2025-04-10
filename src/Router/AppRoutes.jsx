203add2087237473a66094b360ced925b099eee8
Himanshu sahu <sahuhimanshu005@gmail.com>
2025-04-11 00:32:18 +0530

    updated router directory

---
+++ src/Router/AppRoutes.jsx
@@ -0,0 +1,15 @@
+// src/routes/AppRoutes.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
