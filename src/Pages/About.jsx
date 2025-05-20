59201ad4252d9853db3aa691cdc0774e44c9edce
Himanshu sahu <142593637+himanshusahu-07@users.noreply.github.com>
2025-05-21 02:56:45 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,1 +1,25 @@
 
+// src/pages/About.jsx
+import React from "react";
+import { Helmet } from "react-helmet";
+import AboutInfo from "../components/About/AboutInfo"; 
+import Contact from "../components/About/Contact";
+
+const About = () => {
+  return (
+    <section className="min-h-screen text-white pt-24 px-6 flex flex-col items-center justify-center font-[Poppins] text-center">
+      <Helmet>
+        <title>About | TechQuanta</title>
+        <meta
+          name="description"
+          content="Engage in ......"
+        />
+      </Helmet>
+      <AboutInfo />
+      <Contact />
+      {/* Tagline */}
+    </section>
+  );
+};
+
+export default About;
